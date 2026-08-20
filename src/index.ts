d7a3c83d765ae822aae05c2c1f5faf18fc51f4eb
creation <creation2224@gmail.com>
2026-08-20 12:18:40 +0530

    session 1

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function greet(person : string):string{
+    return `hello ${person}` ; 
+}
+console.log(greet("srajan")) ; 
+
+const username : string = "chai or script";
+
+console.log(greet(username)) ;
