00a2c40019d1e4eef686439743cf3d45fd2a303e
creation <creation2224@gmail.com>
2026-09-14 23:45:33 +0530

    day1

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `hello ${name} `;
+}
+console.log(greet("world"));
+const username = "srajan";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
